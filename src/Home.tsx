3f55c8d28bb955da724f7f09235da8cfe01b5947
Rostyslav Moroziuk <rostyslav.moroziuk@rolique.io>
2021-04-02 12:09:42 +0300

    Integrate electron

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="page">
-      <h1>This is HOME page</h1>
+      <h1>This is HOME page!!!</h1>
       <Link to='/about'>Go to ABOUT</Link>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory, createHashHistory } from 'history';
+import { isElectron } from './utils';
 import Home from './Home';
 import About from './About';
 import './index.css';
 
+export const history = isElectron()
+  ? createHashHistory()
+  : createBrowserHistory();
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router history={history}>
       <Switch>
         <Route path='/' exact>
           <Home />
